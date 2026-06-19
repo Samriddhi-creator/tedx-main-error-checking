ce93a7c444c4d6a41b42ec04bf8b8395b8ddc158
shreya300907 <kumarishreya300907@gmail.com>
2026-06-19 15:47:38 +0530

    feat(themes): three theme pages added with some small fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,7 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { useRouter } from "next/navigation";
 import { FaInstagram, FaLinkedinIn, FaFacebookF, FaXTwitter } from "react-icons/fa6";
-import { useRouter } from "next/navigation";
 
 const navLinks = [
     { label: "Home", href: "/" },
