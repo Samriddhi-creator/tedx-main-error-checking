f2df250aa5021e660f9ecd266c46202c3601304a
hardikbatwal1505@gmail.com <hardikbatwal1505@gmail.com>
2026-08-03 00:27:21 +0530

    indhan khtm

--- src/components/coming-soon/Video.tsx
+++ src/components/coming-soon/Video.tsx
@@ -1,21 +1,25 @@
-function Video(){
-    return(
-        <div className="flex flex-col items-center justify-center w-full px-0">
-            <h1 className="relative z-10 mx-auto text-[45px] sm:text-[70px] md:text-[75px] lg:text-[100px] xl:text-[110px] font-bebas font-normal tracking-wide text-center text-[#EB0028] uppercase drop-shadow-[0_4px_12px_rgba(0,0,0,0.5)]">introduction video</h1>
-            <div className="w-full sm:w-[96%] md:w-[94%] lg:w-[88%] xl:w-[82%] aspect-square sm:aspect-video min-h-[340px] sm:min-h-0 my-6 sm:my-10">
-                <iframe
-                    className="w-full h-full shadow-[0_0_40px_rgba(235,0,40,0.25)] border border-red-900/30"
-                    src="https://www.youtube.com/embed/eVFzbxmKNUw"
-                    title="Introduction Video"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                ></iframe>
+function Video() {
+    return (
+        <div className="flex flex-col items-center justify-center w-full px-4 sm:px-6 md:px-8">
+            <h1 className="relative z-10 mx-auto text-[45px] sm:text-[70px] md:text-[75px] lg:text-[100px] xl:text-[110px] font-bebas font-normal tracking-wide text-center text-[#EB0028] uppercase drop-shadow-[0_4px_12px_rgba(0,0,0,0.5)]">
+                introduction video
+            </h1>
+            <div className="w-full max-w-6xl aspect-video my-6 sm:my-10 rounded-xl overflow-hidden shadow-[0_0_40px_rgba(235,0,40,0.25)] border border-red-900/30 bg-black">
+                <video
+                    className="w-full h-full object-cover"
+                    controls
+                    playsInline
+                    preload="metadata"
+                >
+                    <source src="/intro.mov" type="video/quicktime" />
+                    <source src="/intro.mov" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
             </div>
         </div>
-    )
+    );
 }
 
-export default Video
-// 1920  1080
+export default Video;
+
 
